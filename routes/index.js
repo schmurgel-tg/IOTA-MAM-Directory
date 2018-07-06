cf38b23ef362b6986fc5c728721459b12b339980
schmurgel-tg <marcus.kublank@gmx.de>
2018-07-06 23:05:12 +0200

    Pagesize selection

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var Address = require('../models/address');
 var possibleSorts = ['Title', 'Create', 'Website', 'Description'];
 var PageSize = 10;
 var CurrentPage = 1;
+var LastCount = 0;
 var SortBy = '';
 /* GET home page. */
 // router.get('/', function (req, res, next) {
@@ -34,6 +35,10 @@ router.get('/', function (req, res, next) {
     var pageSize = req.query.pageSize;
     if (isNaN(pageSize) == false) {
         PageSize = parseInt(pageSize);
+        if (LastCount !== 0 && CurrentPage * PageSize > LastCount) {
+            CurrentPage = Math.ceil(LastCount / PageSize);
+            console.log('Reset CurrentPage to:' + CurrentPage);
+        }
         console.log('Set PageSize to:' + pageSize);
     }
     var page = req.query.page;
@@ -41,6 +46,7 @@ router.get('/', function (req, res, next) {
         CurrentPage = parseInt(page);
         console.log('Set CurrentPage to: ' + page);
     }
+    //res.redirect(req.path);
     next();
 });
 
@@ -54,10 +60,12 @@ router.get('/', function (req, res, next) {
         .exec(function (err, addresses) {
             Address.countDocuments().exec(function (err, count) {
                 if (err) return next(err)
+                LastCount = count;
                 res.render('index', {
                     addresslist: addresses,
                     current: CurrentPage,
-                    pages: Math.ceil(count / PageSize)
+                    pages: Math.ceil(count / PageSize),
+                    pageSize: PageSize
                 })
             })
         })
